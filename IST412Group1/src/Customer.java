9d22e950cd978996bf66f9906e758af15a3fcda7
bhworley14 <114296602+bhworley14@users.noreply.github.com>
2022-09-28 18:23:42 -0500

    Add files via upload

---
+++ IST412Group1/src/Customer.java
@@ -0,0 +1,21 @@
+/**
+ * This class contains the basic customer information
+ * Variables are:
+ * firstName: First Name
+ * lastName: Last Name
+ * DOB: Date of Birth
+ * custNum: Individual Customer Number
+ * 
+ */
+
+/**
+ *
+ * @author brandonworley
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private int custNum;
+    private String DOB;
+    
+}
