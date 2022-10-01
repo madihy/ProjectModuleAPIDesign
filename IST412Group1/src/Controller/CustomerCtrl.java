2c9ba5fc946fb60928b2138bb0e09205ca529c40
bhworley14 <114296602+bhworley14@users.noreply.github.com>
2022-10-01 18:49:09 -0500

    Add files via upload

--- IST412Group1/src/Controller/CustomerCtrl.java
+++ IST412Group1/src/Controller/CustomerCtrl.java
@@ -9,17 +9,25 @@ public class CustomerCtrl {
     private long custID;
     
     /**
-     * Assign a unique customer ID number to each customer
+     * Generate and assign a unique customer ID number to each customer
      * @return A long representing the new customer ID number
      */
-    public long assignCustID() {
+    public long generateCustID() {
         return custID;
     }
 
+    /**
+     * Get the customer ID number
+     * @return A long representing the customer ID number
+     */
     public long getCustID() {
         return custID;
     }
 
+    /**
+     * Set the unique customer ID number
+     * @param custID Sets the unique customer ID number
+     */
     public void setCustID(long custID) {
         this.custID = custID;
     }
