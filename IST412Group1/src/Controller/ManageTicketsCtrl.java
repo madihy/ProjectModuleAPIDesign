0d228392bdb34c914945adc922c1dff82ba7ea56
kmrineer <kmrineer@gmail.com>
2022-10-02 19:06:04 -0400

    Create ManageTicketsCtrl.java
    Created and framed out methods for open ticket and assign ticket actions.

---
+++ IST412Group1/src/Controller/ManageTicketsCtrl.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author katerineer
+ * This class is used to manage the tickets that the customer has purchased.
+ * It allows the customer to view a list of tickets, open individual tickets, and assign the ticket to a person.
+ */
+public class ManageTicketsCtrl {
+    
+    /**
+     * This is the default constructor for the ManageTicketsCtrl class
+     */
+    public ManageTicketsCtrl(){
+    
+    }
+     /**
+     * Establishes the action event to be carried out when the open ticket button 
+     * is selected on the manage tickets user interface.
+     * @param event sets the event variable to occur when the open ticket button is selected.
+     */
+    public void openTicketsButtonEvent (ActionEvent event){
+        
+    }
+    
+    /**
+     * Establishes the action event to be carried out when the assign ticket button 
+     * is selected on the manage tickets user interface.
+     * @param event sets the event variable to occur when the assign ticket button is selected.
+     */
+    public void assignTicketsButtonEvent (ActionEvent event){
+        
+    }
+        
+    
+    
+}
