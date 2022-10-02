98aa44827e0a8446675d1decaadab75151c61d07
kmrineer <kmrineer@gmail.com>
2022-10-02 18:22:23 -0400

    Create LoginCtrl.java
    Created the LoginCtrl class with a couple of methods

---
+++ IST412Group1/src/Controller/LoginCtrl.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author Kate
+ * This class manages the actions to facilitate the login process for all users of this application.
+ */
+public class LoginCtrl {
+    
+    /**
+     * This is the default constructor for the LoginCtrl class.
+     */
+    
+    public LoginCtrl(){
+        
+    }
+    /**
+     * Establishes the action event to be carried out when the submit button is selected on the login screen.
+     * Authenticates the user name and password from the list of accounts stored in a data file.
+     * Throws an exception and provides an message on the screen if the login information is not found.
+     * @param event sets the event variable to occur when the submit button is selected on the login screen.
+     */
+    private void handleSubmitButtonAction(ActionEvent event){
+        
+    }
+    /**
+     * Establishes the action event to be carried out when the new user button is selected on the login screen.
+     * Directs the user to the new user login screen to set up a user account.
+     * @param event sets the event variable to occur when the new user button is selected on the login screen.
+     */
+    private void handleNewUserButtonAction(ActionEvent event){
+        
+    }
+    
+}
