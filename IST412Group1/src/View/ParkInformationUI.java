e6a1f193cbeb33738a73c24d550c427a23a62507
kmrineer <kmrineer@gmail.com>
2022-10-12 00:08:02 -0400

    Create ParkInformationUI.java
    created basic frame for the ParkInformationUI class.

---
+++ IST412Group1/src/View/ParkInformationUI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ * This is class to support the user interface for accessing park information about park locations, hours, 
+ * rides, restaurants, and shows.
+ * @author katerineer
+ */
+public class ParkInformationUI {
+    
+    /**
+     * This is the default constructor for the ParkInformationUI class.
+     */
+    
+    public ParkInformationUI(){
+    
+    }
+}
