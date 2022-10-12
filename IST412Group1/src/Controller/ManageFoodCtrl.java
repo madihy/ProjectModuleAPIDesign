59b7022cef8bfc8d46187ebc050f3168d7652be0
kmrineer <kmrineer@gmail.com>
2022-10-12 00:57:19 -0400

    OrderFoodUI and ManageFoodCtrl
    Added just the basic default constructors for the OrderFoodUI class and the ManageFoodCtrl class.  Minimal documentation about the classes purpose.

---
+++ IST412Group1/src/Controller/ManageFoodCtrl.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ * This class is to manage the actions to place and manage food orders at the park.
+ * @author katerineer
+ */
+public class ManageFoodCtrl {
+    
+    /**
+     * This is the default controller for the ManageFoodCtrl class.
+     */
+    public ManageFoodCtrl(){
+        
+    }
+    
+}
---
+++ IST412Group1/src/View/OrderFoodUI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ * This class is the user interface for the customer to be able to order food at the park.
+ * @author Kate
+ */
+public class OrderFoodUI {
+    
+    /**
+     * This is the default constructor for the OrderFoodUI class.
+     */
+    public OrderFoodUI(){
+        
+    }
+    
+}
