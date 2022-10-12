2f5d91ce346037e426f883a3660fa652715f7c85
hytham <hytham.madi@outlook.com>
2022-10-12 16:58:16 -0700

    Forms UI

--- IST412Group1/src/View/ReportsUI.java
+++ IST412Group1/src/View/ReportsUI.java
@@ -6,5 +6,64 @@ package View;
  */
 
 public class ReportsUI {
+    // Variables declaration
+    private javax.swing.JButton btnApprove;
+    private javax.swing.JButton btnCreate;
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnModify;
+    private javax.swing.JButton btnReturn;
+    private javax.swing.JButton btnSubmit;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblReports;
+    // End of variables declaration
 
+    /**
+     * Creates new form ReportsUI
+     */
+    public ReportsUI() {
+        initComponents();
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the report.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblReports = new javax.swing.JTable();
+        btnCreate = new javax.swing.JButton();
+        btnModify = new javax.swing.JButton();
+        btnSubmit = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnReturn = new javax.swing.JButton();
+        btnApprove = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        tblReports.setModel(new javax.swing.table.DefaultTableModel(
+                new Object[][]{
+
+                },
+                new String[]{
+                        "Report ID", "Report Type", "Report Date", "Report Status", "Report Comments"
+                }
+        ));
+        jScrollPane1.setViewportView(tblReports);
+
+        btnCreate.setText("Create");
+
+        btnModify.setText("Modify");
+
+        btnSubmit.setText("Submit");
+
+        btnDelete.setText("Delete");
+
+        btnReturn.setText("Return");
+
+        btnApprove.setText("Approve");
+
+        jLabel1.setText("Reports");
+
+    }
 }
