e1cd0ba8c152f50529922091df692f0b89259c23
bhworley14 <bhw5089@psu.edu>
2022-11-12 18:51:31 -0600

    Update CreateCustomerCtrl.java
    Adding coding for actionPerformed buttons

--- IST412Group1/src/Controller/CreateCustomerCtrl.java
+++ IST412Group1/src/Controller/CreateCustomerCtrl.java
@@ -1,26 +1,55 @@
 
 package Controller;
 
+import Model.Account;
 import View.CreateCustomerUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author brandonworley
  */
-public class CreateCustomerCtrl {
+public class CreateCustomerCtrl implements ActionListener {
     private String username;
     private String password;
     private String firstName; 
     private String lastName; 
     private String email;
+    private CreateCustomerUI createCust;
     
     public CreateCustomerCtrl() {
-        CreateCustomerUI createCust = new CreateCustomerUI();        
+        createCust = new CreateCustomerUI();   
+        addALButtons();
+        createCust.setVisible(true);
         
     }
     
-    // Add Action Listener for SAVE button
-    
-    // Add Action Listener for CLEAR button
-    
+    public void addALButtons() {
+        createCust.btnSave.addActionListener(this);
+        createCust.btnClear.addActionListener(this);
+    }       
+  
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        if (obj == createCust.btnClear) {
+            createCust.tfFirstName.setText("");
+            createCust.tfLastName.setText("");
+            createCust.tfEmail.setText("");
+            createCust.tfUsername.setText("");
+            createCust.tfPassword.setText("");
+        }
+        if (obj == createCust.btnSave) {
+            firstName = createCust.tfFirstName.getText();
+            lastName = createCust.tfLastName.getText();
+            email = createCust.tfEmail.getText();
+            username = createCust.tfUsername.getText();
+            password = createCust.tfPassword.getText();
+            
+            // Should the account number randomizer go here?
+            // Create a new account object and save it to the users arraylist
+            
+        }
+    }    
 }
